1bb2aeb9766500eae0aa3d5ffeaee4ac54e04a63
sergey <serger777@gmail.com>
2019-05-10 19:57:05 +0300

    polyfil

--- source/_js/main.js
+++ source/_js/main.js
@@ -8,3 +8,12 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     })
 });
+// forEach polyfill
+if ('NodeList' in window && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = (callback, thisArg) => {
+        thisArg = thisArg || window;
+        for (let i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+}
